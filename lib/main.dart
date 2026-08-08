2305f9b69aa1465c41162a2ef686567e1f52657f
Ziad Tamer <225040@eru.edu.eg>
2026-08-08 19:07:04 +0300

    Initialize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:medico/core/utils/app_route.dart';
+import 'package:medico/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Medico());
 }
 
