8c63618c4eab0c1a2533a4aa003ea1b4181c28ef
Ziad Tamer <225040@eru.edu.eg>
2026-08-09 01:35:30 +0300

    Create Login Cubit

--- lib/features/auth/presentation/manager/login_cubit/login_cubit.dart
+++ lib/features/auth/presentation/manager/login_cubit/login_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:medico/features/auth/domain/entities/user_entity.dart';
+import 'package:medico/features/auth/domain/repos/auth_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginInitial());
+  LoginCubit({required this.authRepo}) : super(LoginInitial());
+  final AuthRepo authRepo;
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    emit(LoginLoading());
+    var result = await authRepo.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    result.fold(
+      (failure) => emit(LoginFailure(errMessage: failure.errMeesage)),
+      (userEntity) => emit(LoginSuccess(userEntity: userEntity)),
+    );
+  }
 }
