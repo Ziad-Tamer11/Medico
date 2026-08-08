e9df4cec247a9df9f90b626da3dc258cd33b2099
Ziad Tamer <225040@eru.edu.eg>
2026-08-08 21:50:47 +0300

    Create Sign Up Cubit

--- lib/features/auth/presentation/manager/sign_up_cubit/sign_up_cubit.dart
+++ lib/features/auth/presentation/manager/sign_up_cubit/sign_up_cubit.dart
@@ -1,9 +1,28 @@
 import 'package:bloc/bloc.dart';
 import 'package:medico/features/auth/domain/entities/user_entity.dart';
+import 'package:medico/features/auth/domain/usecases/auth_usecase.dart';
 import 'package:meta/meta.dart';
 
 part 'sign_up_state.dart';
 
 class SignUpCubit extends Cubit<SignUpState> {
-  SignUpCubit() : super(SignUpInitial());
+  SignUpCubit({required this.authUsecase}) : super(SignUpInitial());
+  final AuthUsecase authUsecase;
+
+  Future<void> createUserWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    emit(SignUpLoading());
+    var result = await authUsecase.createUserWithEmailAndPassword(
+      name: name,
+      email: email,
+      password: password,
+    );
+    result.fold(
+      (failure) => emit(SignUpFailure(errMessage: failure.errMeesage)),
+      (userEntity) => emit(SignUpSuccess(userEntity: userEntity)),
+    );
+  }
 }
