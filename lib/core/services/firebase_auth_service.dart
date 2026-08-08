2c155da0515b768c8cff2733427d0f58b53dd21a
Ziad Tamer <225040@eru.edu.eg>
2026-08-09 00:48:09 +0300

    Create Sign In With Email And Password

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,9 +1,9 @@
 import 'dart:developer';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:medico/core/errors/exceptions.dart';
 
 class FirebaseAuthService {
+  //create user with email and password
   Future<User> createUserWithEmailAndPassword({
     required String email,
     required String password,
@@ -36,6 +36,43 @@ class FirebaseAuthService {
     } catch (e) {
       log(
         "Exception in FirebaseAuthService.createUserWithEmailAndPassword: ${e.toString()}",
+      );
+      throw CustomExceptions(
+        errMessage: 'Something went wrong. Please try again.',
+      );
+    }
+  }
+
+  //sign in with email and password
+  Future<User> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        throw CustomExceptions(errMessage: 'No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        throw CustomExceptions(errMessage: 'Invalid email or password.');
+      } else if (e.code == 'invalid-credential') {
+        throw CustomExceptions(errMessage: 'Invalid email or password.');
+      } else if (e.code == 'network-request-failed') {
+        throw CustomExceptions(
+          errMessage: 'Make sure you are connected to the internet.',
+        );
+      } else {
+        throw CustomExceptions(
+          errMessage: 'Something went wrong. Please try again.',
+        );
+      }
+    } catch (e) {
+      log(
+        "Exception in FirebaseAuthService.signInWithEmailAndPassword: ${e.toString()}",
       );
       throw CustomExceptions(
         errMessage: 'Something went wrong. Please try again.',
