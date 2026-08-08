46b86fb034e3e2a87ae15322ea3fe2f40784d3c4
Ziad Tamer <225040@eru.edu.eg>
2026-08-08 09:04:50 +0300

    Splash View UI Done

--- lib/features/splash/presentation/view/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/view/widgets/splash_view_body.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:medico/core/utils/app_images.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: SvgPicture.asset(
+        Assets.imagesLogo,
+        height: MediaQuery.of(context).size.height * 0.3,
+      ),
+    );
   }
 }
