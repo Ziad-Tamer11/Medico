bc569bc38d00af2b953ac918dfe3df0fba016e93
Ziad Tamer <225040@eru.edu.eg>
2026-08-09 17:27:24 +0300

    Add FireStrore Service & Create Database Service

---
+++ lib/core/services/fire_store_service.dart
@@ -0,0 +1,12 @@
+import 'package:medico/core/services/database_service.dart';
+
+class FireStoreService implements DatabaseService {
+  @override
+  Future<void> addData({
+    required String path,
+    required Map<String, dynamic> data,
+  }) {
+    // TODO: implement addData
+    throw UnimplementedError();
+  }
+}
