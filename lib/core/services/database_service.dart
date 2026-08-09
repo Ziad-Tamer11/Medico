8f48352606eee28215c055920eb33ae5518048fa
Ziad Tamer <225040@eru.edu.eg>
2026-08-09 17:36:01 +0300

    Implement Add Data Method

--- lib/core/services/database_service.dart
+++ lib/core/services/database_service.dart
@@ -1,4 +1,5 @@
 abstract class DatabaseService {
+  //هنا بنضيف البيانات بتاعتنا في ال فايرستور
   Future<void> addData({
     required String path,
     required Map<String, dynamic> data,
--- lib/core/services/fire_store_service.dart
+++ lib/core/services/fire_store_service.dart
@@ -1,12 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:medico/core/services/database_service.dart';
 
 class FireStoreService implements DatabaseService {
+  //ازاي البياانات دي هيتعملها implement عشان تضاف في الفاير ستور
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  //add data
   @override
   Future<void> addData({
     required String path,
     required Map<String, dynamic> data,
-  }) {
-    // TODO: implement addData
-    throw UnimplementedError();
+  }) async {
+    await firestore.collection(path).add(data);
   }
 }
