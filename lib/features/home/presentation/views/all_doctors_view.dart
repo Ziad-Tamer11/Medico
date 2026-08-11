1a44fd9561d3e5ce0910511a3b3af15b381efc3f
Ziad Tamer <225040@eru.edu.eg>
2026-08-11 20:13:15 +0300

    Add Custom App Bar To All Doctors View

--- lib/features/home/presentation/views/all_doctors_view.dart
+++ lib/features/home/presentation/views/all_doctors_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:medico/core/widgets/build_custom_app_bar.dart';
 
 class AllDoctorsView extends StatelessWidget {
   const AllDoctorsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(appBar: buildCustomAppBar(context, title: 'All Doctors'));
   }
 }
