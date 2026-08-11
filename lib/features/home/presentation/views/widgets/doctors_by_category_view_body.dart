395430ae8c45875b2f53575c5d5050ed4a73ac00
Ziad Tamer <225040@eru.edu.eg>
2026-08-11 19:31:58 +0300

    Add Result Header

--- lib/features/home/presentation/views/widgets/doctors_by_category_view_body.dart
+++ lib/features/home/presentation/views/widgets/doctors_by_category_view_body.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:medico/constants.dart';
+import 'package:medico/core/widgets/result_header.dart';
 
 class DoctorsByCategoryViewBody extends StatelessWidget {
   const DoctorsByCategoryViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+      child: const Column(children: [ResultHeader()]),
+    );
   }
 }
