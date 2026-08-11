6da5f7c4056ae8fd7bc78ba48e9c754fa78a0917
Ziad Tamer <225040@eru.edu.eg>
2026-08-11 19:31:10 +0300

    Add Build Custom App Bar

--- lib/features/home/presentation/views/doctors_by_category_view.dart
+++ lib/features/home/presentation/views/doctors_by_category_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:medico/core/widgets/build_custom_app_bar.dart';
+import 'package:medico/features/home/presentation/views/widgets/doctors_by_category_view_body.dart';
 
 class DoctorsByCategoryView extends StatelessWidget {
   const DoctorsByCategoryView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: buildCustomAppBar(context, title: 'Heart'),
+      body: DoctorsByCategoryViewBody(),
+    );
   }
 }
---
+++ lib/features/home/presentation/views/widgets/doctors_by_category_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DoctorsByCategoryViewBody extends StatelessWidget {
+  const DoctorsByCategoryViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
