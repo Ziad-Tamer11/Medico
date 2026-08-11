98cf4ba6869b4e04082854534f3ce1573606bd82
Ziad Tamer <225040@eru.edu.eg>
2026-08-11 20:17:22 +0300

    Add Doctors Card List View To All Doctors View Body

--- lib/features/home/presentation/views/all_doctors_view.dart
+++ lib/features/home/presentation/views/all_doctors_view.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:medico/core/widgets/build_custom_app_bar.dart';
+import 'package:medico/features/home/presentation/views/widgets/all_doctors_view_body.dart';
 
 class AllDoctorsView extends StatelessWidget {
   const AllDoctorsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: buildCustomAppBar(context, title: 'All Doctors'));
+    return Scaffold(
+      appBar: buildCustomAppBar(context, title: 'All Doctors'),
+      body: AllDoctorsViewBody(),
+    );
   }
 }
