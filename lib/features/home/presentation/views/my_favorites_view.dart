7acb167962bb6b99c6eeb64640434f2096046a64
Ziad Tamer <225040@eru.edu.eg>
2026-08-12 05:17:27 +0300

    Create My Favorites View Body

--- lib/features/home/presentation/views/my_favorites_view.dart
+++ lib/features/home/presentation/views/my_favorites_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:medico/core/widgets/build_custom_app_bar.dart';
+import 'package:medico/features/home/presentation/views/widgets/my_favorites_view_body.dart';
 
 class MyFavoritesView extends StatelessWidget {
   const MyFavoritesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: buildCustomAppBar(context, title: 'My Favorites'),
+      body: MyFavoritesViewBody(),
+    );
   }
 }
---
+++ lib/features/home/presentation/views/widgets/my_favorites_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyFavoritesViewBody extends StatelessWidget {
+  const MyFavoritesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
