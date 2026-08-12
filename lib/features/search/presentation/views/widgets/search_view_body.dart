39d65013a696abf42f0f7b1a110b84b4e78b04e0
Ziad Tamer <225040@eru.edu.eg>
2026-08-12 18:35:26 +0300

    Search Logic Done

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -4,24 +4,52 @@ import 'package:medico/core/widgets/result_header.dart';
 import 'package:medico/features/home/presentation/views/widgets/no_result.dart';
 import 'package:medico/features/search/presentation/views/widgets/search_text_field.dart';
 
-class SearchViewBody extends StatelessWidget {
+class SearchViewBody extends StatefulWidget {
   const SearchViewBody({super.key});
+
+  @override
+  State<SearchViewBody> createState() => _SearchViewBodyState();
+}
+
+class _SearchViewBodyState extends State<SearchViewBody> {
+  late TextEditingController searchController;
+
+  bool showCloseIcon = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(height: 50),
+        const SizedBox(height: 50),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
           child: SearchTextField(
-            searchController: TextEditingController(),
-            showCloseIcon: true,
+            searchController: searchController,
+            showCloseIcon: showCloseIcon,
+            onChanged: (value) {
+              setState(() {
+                showCloseIcon = value.isNotEmpty;
+              });
+            },
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding),
           child: ResultHeader(),
         ),
         NoResult(text: 'Search for the doctor you want'),
