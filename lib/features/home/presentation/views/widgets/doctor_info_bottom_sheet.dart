64402e0b255ab7789105858bf6a482d1cd422007
Ziad Tamer <225040@eru.edu.eg>
2026-08-13 17:05:05 +0300

    Add Doctor Rating to Doctor Info Bottom Sheet

---
+++ lib/features/home/presentation/views/widgets/doctor_info_bottom_sheet.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:medico/core/utils/app_colors.dart';
+import 'package:medico/core/utils/app_text_styles.dart';
+import 'package:medico/features/home/presentation/views/widgets/doctor_availability.dart';
+import 'package:medico/features/home/presentation/views/widgets/doctor_rating.dart';
+
+class DoctorInfoBottomSheet extends StatelessWidget {
+  const DoctorInfoBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      padding: const EdgeInsets.only(left: 24, right: 24),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // Drag handle
+          Container(
+            width: 64,
+            height: 8,
+            decoration: BoxDecoration(
+              color: AppColor.divider,
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          const SizedBox(height: 32),
+          Column(
+            children: [
+              Row(
+                children: [
+                  Text('David H. Brown', style: TextStyles.font18Bold),
+                  Spacer(),
+                  DoctorRating(textStyle: TextStyles.font14SemiBold),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/doctor_rating.dart
+++ lib/features/home/presentation/views/widgets/doctor_rating.dart
@@ -4,13 +4,14 @@ import 'package:medico/core/utils/app_images.dart';
 import 'package:medico/core/utils/app_text_styles.dart';
 
 class DoctorRating extends StatelessWidget {
-  const DoctorRating({super.key});
+  const DoctorRating({super.key, this.textStyle});
+  final TextStyle? textStyle;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Text('4.8', style: TextStyles.font14Medium),
+        Text('4.8', style: textStyle ?? TextStyles.font14Medium),
         const SizedBox(width: 4),
         SvgPicture.asset(Assets.imagesStar),
       ],
